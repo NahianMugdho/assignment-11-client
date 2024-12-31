93bf0b26afa8ff6e37d098a40e954543864d27ad
NahianMugdho <nahianmugdho@gmail.com>
2025-01-01 01:53:30 +0600

    'lottie'

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import regxAnim from '../assets/Animation.json'
+import Lottie from 'lottie-react';
 
 const Register = () => {
     return (
         <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
+          <div className="px-10 text-center lg:text-left">
             <h1 className="text-5xl font-bold">Register now!</h1>
             <p className="py-6">
-              Join our community today! Create your account to access personalized recommendations and more.
+             <Lottie animationData={regxAnim}></Lottie>
             </p>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
